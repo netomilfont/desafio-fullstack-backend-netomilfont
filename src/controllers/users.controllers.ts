64e7fa6f393a866c08e0c2a07dae4136d7429785
netomilfont <netomilfont@hotmail.com>
2023-03-25 19:18:17 -0300

    feat: add controller of list a user

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -2,10 +2,20 @@ import { Request, Response } from "express";
 import { IUserRequest } from "../interfaces/users.interface";
 import { createUserService } from "../services/users/createUser.service";
 
-const createUserController = (req: Request, res: Response) => {
+const createUserController = async (req: Request, res: Response) => {
   const userData: IUserRequest = req.body;
   const newUser = await createUserService(userData);
   return res.status(201).json(newUser);
+};
+
+const listUsersController = async (req: Request, res: Response) => {
+  const users = await listUsersService();
+  return res.status(200).json(users);
+};
+
+const listUserController = async (req: Request, res: Response) => {
+  const user = await listUserService(res.locals.foundUser, req.user.id);
+  return res.status(200).json(user);
 };
 
 export { createUserController };
