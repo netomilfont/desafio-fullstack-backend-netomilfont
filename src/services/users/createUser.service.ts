d87a7fd24efb32f5d9f1a1816462deab8f57c352
netomilfont <netomilfont@hotmail.com>
2023-03-25 19:06:46 -0300

    feat: create service of create user

--- src/services/users/createUser.service.ts
+++ src/services/users/createUser.service.ts
@@ -1,4 +1,16 @@
 import { AppDataSource } from "../../data-source";
 import { User } from "../../entities/user.entity";
+import { IUserRequest } from "../../interfaces/users.interface";
 
-const createUserService = () => {};
+const createUserService = async (userData: IUserRequest): Promise<User> => {
+  const userRepository = AppDataSource.getRepository(User);
+
+  const findUser = await userRepository.findOneBy({
+    email: userData.email,
+  });
+
+  const user = userRepository.create(userData);
+  await userRepository.save(user);
+
+  return user;
+};
