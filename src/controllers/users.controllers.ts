b5166e1bd41340ff6e5b81478722e8e53d3a49b1
netomilfont <netomilfont@hotmail.com>
2023-03-25 19:26:33 -0300

    feat: add controller of delete a user

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -19,15 +19,17 @@ const listUserController = async (req: Request, res: Response) => {
 };
 
 const updateUserController = async (req: Request, res: Response) => {
-  const newUserData = req.body;
-  const foundUser = res.locals.foundUser;
-
-  const updatedUser = await updateUserService(
-    newUserData,
-    foundUser,
-    req.params.id
-  );
+  const userData = req.body;
+  const userDataId: string = req.user.id;
+  const userId: string = req.params.id;
+  const updatedUser = await updateUserService(userData, userId, userDataId);
   return res.status(200).json(updatedUser);
+};
+
+const deleteUserController = async (req: Request, res: Response) => {
+  const userId: string = req.params.id;
+  const deleteUser = await deleteUserService(userId);
+  return res.status(204).json(deleteUser);
 };
 
 export {
