a72ccebbe3bd6bed7190b1da487c49edf4fba220
netomilfont <netomilfont@hotmail.com>
2023-03-26 19:38:50 -0300

    feat: able to create a contact

--- src/controllers/contacts.controllers.ts
+++ src/controllers/contacts.controllers.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
+import { IContactRequest } from "../interfaces/contacts.interface";
 import createContactService from "../services/contacts/createContact.service";
 
 const createContactController = async (req: Request, res: Response) => {
-  const contactData = req.body;
-  const userId = req.user.id;
+  const contactData: IContactRequest = req.body;
+  const userId: string = req.user.id;
   const createdContact = await createContactService(contactData, userId);
   return res.status(201).json(createdContact);
 };
--- src/services/contacts/createContact.service.ts
+++ src/services/contacts/createContact.service.ts
@@ -1,21 +1,23 @@
 import AppDataSource from "../../data-source";
 import { Contact } from "../../entities/contact.entity";
 import { User } from "../../entities/user.entity";
+import { AppError } from "../../errors/appErrors";
+import { IContactRequest } from "../../interfaces/contacts.interface";
 
 const createContactService = async (
-  contactData,
+  contactData: IContactRequest,
   userId: string
 ): Promise<Contact> => {
   const userRepository = AppDataSource.getRepository(User);
   const contactRepository = AppDataSource.getRepository(Contact);
 
-  const user = userRepository.findOneBy({
+  const user = await userRepository.findOneBy({
     id: userId,
   });
 
   const contact = contactRepository.create({
     ...contactData,
-    user,
+    user: user!,
   });
 
   await contactRepository.save(contact);
