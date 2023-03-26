b38369e5add6cf9c633335b6747a5de03958387a
netomilfont <netomilfont@hotmail.com>
2023-03-26 14:23:47 -0300

    feat: add session routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata";
 import express from "express";
 import userRoutes from "./routes/users.routes";
+import sessionRoutes from "./routes/sessions.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use("/users", userRoutes);
+app.use("/login", sessionRoutes);
 
 export default app;
