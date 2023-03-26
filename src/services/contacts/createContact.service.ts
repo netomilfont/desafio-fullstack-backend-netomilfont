23229e624d6c146384de9ba8b89b78aee3a43634
netomilfont <netomilfont@hotmail.com>
2023-03-26 19:22:07 -0300

    feat: create a interface of contact

--- src/services/contacts/createContact.service.ts
+++ src/services/contacts/createContact.service.ts
@@ -5,6 +5,22 @@ import { User } from "../../entities/user.entity";
 const createContactService = async (
   contactData,
   userId: string
-): Promise<Contact> => {};
+): Promise<Contact> => {
+  const userRepository = AppDataSource.getRepository(User);
+  const contactRepository = AppDataSource.getRepository(Contact);
+
+  const user = userRepository.findOneBy({
+    id: userId,
+  });
+
+  const contact = contactRepository.create({
+    ...contactData,
+    user,
+  });
+
+  await contactRepository.save(contact);
+
+  return contact;
+};
 
 export default createContactService;
