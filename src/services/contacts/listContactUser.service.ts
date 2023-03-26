4668edfbc8066a78efc8f22baf1d10cfec7c6ccf
netomilfont <netomilfont@hotmail.com>
2023-03-26 20:26:55 -0300

    feat: able to list an user contacts

--- src/services/contacts/listContactUser.service.ts
+++ src/services/contacts/listContactUser.service.ts
@@ -2,6 +2,19 @@ import AppDataSource from "../../data-source";
 import { Contact } from "../../entities/contact.entity";
 import { User } from "../../entities/user.entity";
 
-const listContactUserService = async (userId: string): Promise<Contact[]> => {};
+const listContactUserService = async (userId: string): Promise<Contact[]> => {
+  const userRepository = AppDataSource.getRepository(User);
+
+  const user = await userRepository.findOne({
+    where: {
+      id: userId,
+    },
+    relations: {
+      contacts: true,
+    },
+  });
+
+  return user!.contacts!;
+};
 
 export default listContactUserService;
