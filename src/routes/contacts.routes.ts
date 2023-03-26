9db73ef8e4912a38d2a3d8dfdace3d0dde62cd81
netomilfont <netomilfont@hotmail.com>
2023-03-26 19:11:26 -0300

    feat: add service of create a contact

--- src/routes/contacts.routes.ts
+++ src/routes/contacts.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { createContactController } from "../controllers/contacts.controllers";
 import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware";
 
 const contactRoutes = Router();
 
-contactRoutes.post("", ensureAuthMiddleware);
+contactRoutes.post("", ensureAuthMiddleware, createContactController);
 
 export default contactRoutes;
---
+++ src/services/contacts/createContact.service.ts
@@ -0,0 +1,10 @@
+import AppDataSource from "../../data-source";
+import { Contact } from "../../entities/contact.entity";
+import { User } from "../../entities/user.entity";
+
+const createContactService = async (
+  contactData,
+  userId: string
+): Promise<Contact> => {};
+
+export default createContactService;
