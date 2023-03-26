1e0119fdd316c7970b2b3e8e6c6c27b8d4659f41
netomilfont <netomilfont@hotmail.com>
2023-03-26 19:21:24 -0300

    feat: create a controller of create contact

--- src/controllers/contacts.controllers.ts
+++ src/controllers/contacts.controllers.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from "express";
+import createContactService from "../services/contacts/createContact.service";
 
-const createContactController = async (req: Request, res: Response) => {};
+const createContactController = async (req: Request, res: Response) => {
+  const contactData = req.body;
+  const userId = req.user.id;
+  const createdContact = await createContactService(contactData, userId);
+  return res.status(201).json(createdContact);
+};
 
 export { createContactController };
