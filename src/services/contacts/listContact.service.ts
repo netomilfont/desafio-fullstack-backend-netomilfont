7496716f6c7162992658a0e3b4bca59c86c65e52
netomilfont <netomilfont@hotmail.com>
2023-03-26 21:19:54 -0300

    feat: add a service of list a specific contact

--- src/services/contacts/listContact.service.ts
+++ src/services/contacts/listContact.service.ts
@@ -1,10 +1,37 @@
 import AppDataSource from "../../data-source";
 import { Contact } from "../../entities/contact.entity";
 import { User } from "../../entities/user.entity";
+import { AppError } from "../../errors/appErrors";
 
 const listContactService = async (
   userId: string,
   contactId: string
-): Promise<Contact> => {};
+): Promise<Contact> => {
+  const userRepository = AppDataSource.getRepository(User);
+  const contactRepository = AppDataSource.getRepository(Contact);
+
+  const user = await userRepository.findOneBy({
+    id: userId,
+  });
+
+  const contact = await contactRepository.findOne({
+    where: {
+      id: contactId,
+    },
+    relations: {
+      user: true,
+    },
+  });
+
+  if (!contact) {
+    throw new AppError("Contact not found!", 404);
+  }
+
+  if (contact.user.id !== user!.id) {
+    throw new AppError("You don't have permission to see this contact", 403);
+  }
+
+  return contact;
+};
 
 export default listContactService;
