5c230ec9222c4f3e918a598464f9104c554a12ed
netomilfont <netomilfont@hotmail.com>
2023-03-26 22:39:03 -0300

    feat: add a service of delete a contact

--- src/services/contacts/deleteContactUser.service.ts
+++ src/services/contacts/deleteContactUser.service.ts
@@ -1,7 +1,39 @@
 import AppDataSource from "../../data-source";
 import { Contact } from "../../entities/contact.entity";
 import { User } from "../../entities/user.entity";
+import { AppError } from "../../errors/appErrors";
 
-const deleteContactUserService = async () => {};
+const deleteContactUserService = async (
+  userId: string,
+  contactId: string
+): Promise<{}> => {
+  const userRepository = AppDataSource.getRepository(User);
+  const contactRepository = AppDataSource.getRepository(Contact);
+
+  const user = await userRepository.findOneBy({
+    id: userId,
+  });
+
+  const contact = await contactRepository.findOne({
+    where: {
+      id: contactId,
+    },
+    relations: {
+      user: true,
+    },
+  });
+
+  if (!contact) {
+    throw new AppError("Contact not found!", 404);
+  }
+
+  if (contact.user.id !== user!.id) {
+    throw new AppError("You don't have permission to see this contact", 403);
+  }
+
+  await contactRepository.delete(contact.id);
+
+  return {};
+};
 
 export default deleteContactUserService;
